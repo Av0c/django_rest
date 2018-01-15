dae1ca26332903eafa15b3d45d4032df9c309d2f
Av0c <mindstorm1998@gmail.com>
2018-01-15 12:38:46 +0200

    Add virtualenv

---
+++ mysite/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/mnt/c/Users/Av0c/Desktop/VAMK/Databases/REST/django_rest_mysql/mysite/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
